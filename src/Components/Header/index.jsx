671efbfaed193f581a957032d4476d551745a0e9
Herbert Wander "Dragon <herbert.starcraft2@gmail.com>
2022-06-07 15:26:06 -0300

    Home Page implementation

--- src/Components/Header/index.jsx
+++ src/Components/Header/index.jsx
@@ -3,7 +3,7 @@ export function Header()
 {
     return (
         <nav>
-            <img src="./src/images/pokemonLogo.svg" alt="" />
+            <img src="./src/images/pokemonLogo.svg" alt="Pokemon Logo" />
             <ul>
                 <li><a href="" className="selectNavBar">home</a></li>
                 <li><a href="">pokemons</a></li>
---
+++ src/Pages/Pokemons/Index.jsx
@@ -0,0 +1,19 @@
+import './styles.css';
+import { Header } from '../../Components/Header'
+
+export function Home()
+{
+    return (
+        <section id="page">
+            <Header />
+            <section id="homePage">
+                <div className="presentationBox">
+                    <h1>Qual pokemon<br /><span className="yellowHighLight">você escolheria?</span></h1>
+                    <p>Você pode saber o tipo de Pokémon, seus pontos fortes, fracos e habilidades.</p>
+                    <button>Veja os pokemons</button>
+                </div>
+                <img src="./src/images/banner.svg" alt="Pokemon Banner" className="banner" />
+            </section>
+        </section>
+    )
+}
